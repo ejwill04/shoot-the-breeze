90d45dd9d3f64835f4158b6156bd2e4fb815f13e
Noah Peden <noahpeden@gmail.com>
2016-12-19 17:59:19 -0700

    have a couple of basic passing tests and add full search functionality

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -4,6 +4,7 @@ import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
 
 import Application from '../lib/components/Application';
+import {RenderMessages} from '../lib/components/RenderMessages';
 
 describe('Application', () => {
 
@@ -13,3 +14,21 @@ describe('Application', () => {
   });
 
 });
+
+describe('RenderMessages', () => {
+  let messages = [{id: Date.now(), input: 'this is a message' }];
+  let user = {displayName: 'noah'};
+  it('renders three <p> elements', () => {
+    const wrapper = shallow(<RenderMessages
+                                messages={messages}
+                                user={user}/>)
+    assert.equal(wrapper.find('p').length, 3);
+  })
+
+  it('renders current input', () => {
+    const wrapper = shallow(<RenderMessages
+                                messages={messages}
+                                user={user}/>)
+    assert.equal(wrapper.find('.message-input').text(), messages[0].input);
+  })
+})
