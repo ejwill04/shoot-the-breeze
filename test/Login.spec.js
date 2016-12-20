b4fc5cca1e7f215d63c8d6a96080a79c946bf346
Elijah Williams <ejwill04@gmail.com>
2016-12-20 15:49:21 -0700

    more testing

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import sinon from 'sinon';
 
 import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
@@ -6,12 +7,17 @@ import { assert } from 'chai';
 import {Login} from '../lib/components/Login';
 
 describe('Login', () => {
-  const obj = { user: { displayName: 'noah' }, setUser: 'set', text: 'text', authorizer: 'authorize' }
+  const obj = {
+    user: { displayName: 'noah' },
+    setUser: 'set',
+    text: 'text',
+    authorizer: 'authorize' };
+
   const wrapper = shallow(<Login
     user={obj.user}
     setUser={obj.setUser}
     text={obj.text}
-    authorize={obj.authorize}/>)
+    authorize={obj.authorize}/>);
 
   it('returns a div with className of loggedInAs if given a user', () => {
     assert.equal(wrapper.find('.loggedInAs').length, 1);
@@ -22,7 +28,32 @@ describe('Login', () => {
   });
 
   it('renders the users email if given a user', () => {
-    assert.equal(wrapper.find('.usersName').text(), 'noah' );
+    assert.equal(wrapper.find('.usersName').text(), ' noah ');
   });
+
+  it('should not have className of loggedInAs if no user', () => {
+    const wrapper = shallow(<Login
+      setUser={obj.setUser}
+      text={obj.text}
+      authorize={obj.authorize}/>);
+    assert.equal(wrapper.find('.loggedInAs').length, 0);
+  });
+
+  it('should have className of logInBar if no user', () => {
+    const wrapper = shallow(<Login
+      setUser={obj.setUser}
+      text={obj.text}
+      authorize={obj.authorize}/>);
+    assert.equal(wrapper.find('.logInBar').length, 1);
+  });
+
+  // it('simulates click events', () => {
+  //   const onButtonClick = sinon.spy();
+  //   const wrapper = shallow(
+  //     <Login onButtonClick={onButtonClick} />
+  //   );
+  //   wrapper.find('button').simulate('click');
+  //   expect(onButtonClick).to.have.property('callCount', 1);
+  // });
 
 });
